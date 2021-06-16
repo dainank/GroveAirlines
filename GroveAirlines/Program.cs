a9a810f0cb2ab4ab6f0ffe8e345c6f4e3b5f6f85
Benjamin. L. Whelan <whelbe@icloud.com>
2021-06-17 00:05:11 +0200

    Prerequisite Files
    - custom setup for .NET 5 application

---
+++ GroveAirlines/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace GroveAirlines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InitalizeHost();
+        }
+
+        private static void InitalizeHost() =>
+          Host.CreateDefaultBuilder()   // HOST BUILDER (with defaults)
+            .ConfigureWebHostDefaults(builder =>
+            {
+                builder.UseStartup<Startup>();
+                builder.UseUrls("http://0.0.0.0:8080"); // point to URL/PORT
+            }).Build().Run();
+    }
+}
---
+++ GroveAirlines/Startup.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroveAirlines
+{
+    class Startup
+    {
+        public void Configure(IApplicationBuilder app)  // called by HostBuilder (enables usage of controllers/endpoints)
+        {
+            app.UseRouting();   // routing & routing decisions for the service
+            app.UseEndpoints(endpoints => endpoints.MapControllers());  // endpoint pattern for routing web requests (maps all controllers in our service)
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+    }
+}
